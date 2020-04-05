b0ea8c2d390163b0e4aadc833641842d374bd543
makoshika <buryc405@bucs.dawson.edu>
2020-04-05 10:08:11 -0600

    clean up contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -9,35 +9,44 @@ export default class Contact extends Component {
     }
     render() {
         return (
-            <div className="grid-wrapper"
-                className="contact-background"
+            <div className="contact-background"
+               
                 style={{
                     backgroundImage: `url(${contactBackgroundImage})`
                 }}>
+                
                 <div className="contact-wrapper">
             
         
                 {/* <Link to="/contact"></Link> */}
-                <div className="left-wrapper">
-                <img 
-                    className="logo-wrapper" src={logoImage}></img>
-                </div>
+                    <div className="left-wrapper">
+                        <img 
+                            className="logo-wrapper" src={logoImage}></img>
+                    </div>
+
                 <div className="right-wrapper">
 
                     <div className="contact-wrapper-info">
                     </div>
 
-                    <div className="address-wrapper">101 N Merrill Ave
-                    <br/>Glendive, Montana<br/>59330<br/><br/>Phone: 406-377-7977
-                    
+
+                    <div className="address-wrapper">
+                        101 N Merrill Ave
+                        <br/>Glendive, Montana<br/>59330<br/>
+                        <br/>Phone: 406-377-7977
                    </div>
+
                     <div className="email-wrapper">
-                    <button><a target="_blank" 
-                    href= "mailto:hellcreek@midrivers.com?subject=Ladies Uke Night!">
-                        Email: hellcreekmusic@midrivers.com</a></button>
+                        <button>
+                            <a target="_blank" 
+                                href= "mailto:hellcreek@midrivers.com?subject=Ladies Uke Night!">
+                                Email: hellcreekmusic@midrivers.com
+                            </a>
+                        </button>
                    </div>
+
                    </div>
-                   </div> 
+                </div> 
 
             </div>
         );
